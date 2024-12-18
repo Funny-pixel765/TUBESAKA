243677809987e094c821a9e6494cf962c1283d11
Funny-pixel765 <funnyrnd92@gmail.com>
2024-12-18 10:20:27 +0700

    Edit function

---
+++ src/main/java/com/mycompany/datacoklat/DataCoklat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datacoklat;
+
+/**
+ *
+ * @author Lenovo Yoga
+ */
+public class DataCoklat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
